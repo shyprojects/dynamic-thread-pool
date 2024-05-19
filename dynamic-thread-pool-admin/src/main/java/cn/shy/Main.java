8b3f7dc8c4eb0b0c6151bf884bf24e1fa7b55124
shy <3064267442@qq.com>
2024-05-19 14:26:56 +0800

    feat: 项目初始化

---
+++ dynamic-thread-pool-admin/src/main/java/cn/shy/Main.java
@@ -0,0 +1,11 @@
+package cn.shy;
+
+/**
+ * @author shy
+ * @since 2024/5/19 14:07
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
